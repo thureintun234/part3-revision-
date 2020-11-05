c3cb00876a8c36e0ad7e48b556236be0ab4b9ad5
thureintun234 <thurein12456@gmail.com>
2020-11-05 14:50:23 +0630

    deploying connect mongo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/api/notes',(request,response) => {
 
 
 //mongodb post
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   // console.log(body)
@@ -39,9 +39,12 @@ app.post('/api/notes', (request, response) => {
     date: new Date(),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
+  note.save()
+  .then(savedNote => savedNote.toJSON())
+  .then(saveAndFormattedNote => {
+  	response.json(saveAndFormattedNote)
   })
+  .catch(error => next(error))
 })
 
 
@@ -55,10 +58,6 @@ app.get('/api/notes/:id', (request,response,next) => {
 		}
 	})
 	.catch(error => next(error))
-	// .catch(error => {
-	// 	console.log(error)
-	// 	response.status(400).send({error:'malformetted id'})
-	// })
 })
 
 
@@ -97,7 +96,9 @@ const errorHandler = (error, request, response, next) => {
 
 	if(error.name === 'CastError') {
 		return response.status(400).send({error: 'malformetted id'})
-	}
+	}else if (error.name === 'ValidationError') {
+    	return response.status(400).json({ error: error.message })
+  	}
 
 	next(error)
 }
